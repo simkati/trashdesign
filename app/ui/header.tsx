62b27bba14ae9d65842fbecd1b6af8df3e30fc93
simkati <simkat@gmx.net>
2024-03-21 23:12:14 +0100

    remove auth from header component

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -6,8 +6,6 @@ import { signOut, auth } from "@/auth";
 import { FaPowerOff } from "react-icons/fa";
 
 export default async function Header() {
-  const authc = await auth();
-
   return (
     <header className="my-5 mx-10">
       <Link href="/" className="inline-block">
@@ -20,22 +18,20 @@ export default async function Header() {
         />
         <p className="text-xl">Régi tárgyak új szerepben</p>
       </Link>
-      {!authc && <Navlinks />}
-      {authc && (
-        <div className="float-right mr-4">
-          <form
-            action={async () => {
-              "use server";
-              await signOut({ redirectTo: "/" });
-            }}
-          >
-            <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:text-green-800 hover:underline md:flex-none md:justify-start md:p-2 md:px-3">
-              <FaPowerOff className="w-6" />
-              <div className="hidden md:block">Kijelentkezés</div>
-            </button>
-          </form>
-        </div>
-      )}
+      <Navlinks />
+      <div className="float-right mr-4">
+        <form
+          action={async () => {
+            "use server";
+            await signOut({ redirectTo: "/" });
+          }}
+        >
+          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:text-green-800 hover:underline md:flex-none md:justify-start md:p-2 md:px-3">
+            <FaPowerOff className="w-6" />
+            <div className="hidden md:block">Kijelentkezés</div>
+          </button>
+        </form>
+      </div>
     </header>
   );
 }
